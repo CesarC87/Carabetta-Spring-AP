d45e6fb7eef67143eabb2754b1ca831a5d64a2ed
César Carabetta <carabetta87@gmail.com>
2022-08-15 20:06:36 -0300

    mejoras

--- src/main/java/com/carabettaspringboottpfinal/ArgProg/Controller/UserController.java
+++ src/main/java/com/carabettaspringboottpfinal/ArgProg/Controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,4 +34,8 @@ public class UserController {
     public void postUser(@RequestBody User us){
         userServ.postUser(us);
     }    
+    @GetMapping
+    public String getData(){
+        return "Hello world";
+    }
 }
